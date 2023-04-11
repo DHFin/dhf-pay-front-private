8df8728e0316da6140de49246b8b0498d71ae9fb
Andrey Krylov <krylov.a@defilab.space>
2023-04-11 20:11:59 +0300

    hotfix btc wallet validation

--- src/modules/curriencies/bitcoin.ts
+++ src/modules/curriencies/bitcoin.ts
@@ -11,7 +11,10 @@ class Bitcoin extends BaseCurrency {
   }
 
   validateWallet(wallet: string): boolean {
-    return /^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$/.test(wallet);
+    console.log('process.env.NEXT_BITCOIN_NET', process.env.NEXT_PUBLIC_BITCOIN_NET);
+    const includesTestnet = /\b((bc|tb)(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})|1[ac-hj-np-z02-9]{8,87})|([13]|[mn2])[a-km-zA-HJ-NP-Z1-9]{25,39})\b/g;
+    const mainnet = /\b(bc(0([ac-hj-np-z02-9]{39}|[ac-hj-np-z02-9]{59})|1[ac-hj-np-z02-9]{8,87})|[13][a-km-zA-HJ-NP-Z1-9]{25,35})\b/g;
+    return new RegExp(process.env.NEXT_PUBLIC_BITCOIN_NET === 'testnet' ? includesTestnet : mainnet).test(wallet);
   }
 }
 
